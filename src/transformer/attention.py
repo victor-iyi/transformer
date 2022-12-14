e0599cbb8e1f82b6b2d58742cbc86034597cc6d2
Victor I. Afolabi <javafolabi@gmail.com>
2022-12-14 21:27:07 +0100

    Add GlobalSelfAttention layer

--- src/transformer/attention.py
+++ src/transformer/attention.py
@@ -9,6 +9,11 @@ class BaseAttention(tf.keras.layers.Layer):
     """Base attention consists of a MultiHeadAttention, LayerNormalization and Add layer."""
 
     def __init__(self, **kwargs) -> None:  # type: ignore[no-untyped-def]
+        """
+        Arguments:
+            See arguments for `tf.keras.layers.MultiHeadAttention`.
+
+        """
         super().__init__()
         self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
         self.layernorm = tf.keras.layers.LayerNormalization()
@@ -22,11 +27,13 @@ class CrossAttention(BaseAttention):
         """Perform a fuzzy, differentiable, vectorized lookup of a query within a context.
 
         Arguments:
-            query (tf.Tensor): Tensor of shape `(..., query_elements, query_depth)`
-            context (tf.Tensor): Tensor of shape `(..., key_elements, key_depth)`
+            query (tf.Tensor): Query vector.
+                Tensor of shape `(..., query_length, query_depth)`
+            context (tf.Tensor): Context (or key) vector.
+                Tensor of shape `(..., context_length, context_depth)`
 
         Returns:
-            tf.Tensor - A tensor of shape `(..., query_elements, key_depth)`
+            tf.Tensor - A tensor of shape `(batch_size, query_length, embdding_dim)`
         """
         attn_output, attn_scores = self.mha(
             query=query,
@@ -37,6 +44,34 @@ class CrossAttention(BaseAttention):
 
         # Cache the attention scores for plotting later.
         self.last_attn_scores = attn_scores
+
+        x = self.add([query, attn_output])
+        x = self.layernorm(x)
+
+        return x
+
+
+class GlobalSelfAttention(BaseAttention):
+    """Global self attention lets every sequence element directly access every
+    other sequence element, with only a few operations, and all the outputs can
+    be computed in parallel.
+
+    The query `x`, is passed to the `MultiHeadAttention` layer as the `query`,
+    `key` & `value`.
+    """
+
+    def call(self, query: tf.Tensor) -> tf.Tensor:
+        """Self attention layer for query vector `x`.
+
+        Arguments:
+            query (tf.Tensor): Tensor or list of Tensor of shape
+                (batch_size, seq_length, embedding_dim)
+
+        Returns:
+            tf.Tensor - A tensor of shape `(batch_size, seq_length, embedding_dim)`
+        """
+        # The query is passed as Q, K & V for self-attention.
+        attn_output = self.mha(query=query, value=query, key=query)
 
         x = self.add([query, attn_output])
         x = self.layernorm(x)

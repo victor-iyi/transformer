f298fb65a5432f5b6d4acd1066b92eaa063c416e
Victor I. Afolabi <javafolabi@gmail.com>
2022-12-15 21:45:14 +0100

    Add Encoder half of the Transformer architecture

--- src/transformer/encoder.py
+++ src/transformer/encoder.py
@@ -4,6 +4,7 @@
 # https://opensource.org/licenses/MIT
 import tensorflow as tf
 from transformer.attention import GlobalSelfAttention
+from transformer.embedding import PositionalEmbedding
 from transformer.feed_forward import FeedForward
 
 
@@ -43,4 +44,64 @@ class EncoderLayer(tf.keras.layers.Layer):
         """
         x = self.self_attention(x)
         x = self.ffn(x)
+        return x
+
+
+class Encoder(tf.keras.layers.Layer):
+    """Transformer's Encoder layer."""
+
+    def __init__(
+        self, *,
+        num_layers: int, d_model: int, num_heads: int,
+        dff: int, vocab_size: int, dropout: float = 0.1,
+    ) -> None:
+        """Encoder half of the Transformer architecture.
+
+        Arguments:
+            num_layers (int): Number of `EncoderLayer`.
+            d_model (int): Embedding dimension.
+            num_heads (int): Number of `MultiHeadAttention` heads.
+            dff (int): Number of neurons for the feed forward layer.
+            vocab_size (int): Vocabulary size.
+            dropout (float, optional): Dropout rate. Defaults to 0.1.
+        """
+        super().__init__()
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.pos_embedding = PositionalEmbedding(
+            vocab_size=vocab_size, d_model=d_model,
+        )
+
+        self.encoder_layers = [
+            EncoderLayer(
+                d_model=d_model,
+                num_heads=num_heads,
+                dff=dff,
+                dropout=dropout,
+            )
+            for _ in range(num_layers)
+        ]
+        self.dropout = tf.keras.layers.Dropout(dropout)
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        """Encoder transforms input of shape `(batch_size, seq_length)`.
+
+        Arguments:
+            x (tf.Tensor): Tensor of shape `(batch_size, seq_len)`.
+
+        Returns:
+            tf.Tensor - Tensor of shape `(batch_size, seq_len, d_model)`.
+        """
+        # `x` is token-IDs shape: (batch, seq_len)
+        x = self.pos_embedding(x)  # Shape (batch_size, seq_len, d_model)
+
+        # Add dropout.
+        x = self.dropout(x)
+
+        # Pass embedding through a stack of encoder layers.
+        for i in range(self.num_layers):
+            x = self.encoder_layers[i](x)
+
+        # Shape: (batch_size, seq_len, d_model)
         return x
--- tests/encoder_test.py
+++ tests/encoder_test.py
@@ -1,11 +1,14 @@
+import numpy as np
 import pytest
 import tensorflow as tf
+from transformer.encoder import Encoder
 from transformer.encoder import EncoderLayer
 
 
 @pytest.mark.parametrize(
     'embed_dim,dff',
     (
+        (128, 1024),
         (512, 2048),
     ),
 )
@@ -20,3 +23,33 @@ def test_encoder_layer(embed_dim: int, dff: int) -> None:
 
     encoder_layer = EncoderLayer(d_model=embed_dim, num_heads=2, dff=dff)
     assert encoder_layer(embedding).shape == embed_shape
+
+
+@pytest.mark.parametrize(
+    'num_layers,seq_len,embed_dim,dff',
+    (
+        (1, 64, 128, 1024),
+        (3, 82, 256, 512),
+    ),
+)
+def test_encoder(
+    num_layers: int,
+    seq_len: int,
+    embed_dim: int,
+    dff: int,
+) -> None:
+    batch_size, num_heads, vocab_size = 32, 8, 10000
+
+    # Sample data (dummy token IDs)
+    data = np.random.randint(vocab_size, size=(batch_size, seq_len))
+
+    # Multiple encoder layers.
+    encoder = Encoder(
+        num_layers=num_layers,
+        d_model=embed_dim,
+        num_heads=num_heads,
+        dff=dff,
+        vocab_size=vocab_size,
+    )
+    output = encoder(data, training=False)
+    assert output.shape == (batch_size, seq_len, embed_dim)

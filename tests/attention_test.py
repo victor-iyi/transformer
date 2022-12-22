64b91f9ae7e468cb7da631bc127d288a6cf309b9
Victor I. Afolabi <javafolabi@gmail.com>
2022-12-22 20:01:03 +0100

    Update input in attention_test.py

--- tests/attention_test.py
+++ tests/attention_test.py
@@ -6,13 +6,17 @@ from transformer.attention import GlobalSelfAttention
 
 
 @pytest.mark.parametrize(
-    'query_length,context_length,embed_dim',
+    'query_length, context_length, embed_dim',
     (
         (71, 78, 128),
         (128, 128, 512),
     ),
 )
-def test_cross_attention(query_length: int, context_length: int, embed_dim: int) -> None:
+def test_cross_attention(
+    query_length: int,
+    context_length: int,
+    embed_dim: int,
+) -> None:
     """Check the shape of the cross attention layer's output."""
 
     # Create dummy query embedding vector.
@@ -36,7 +40,7 @@ def test_cross_attention(query_length: int, context_length: int, embed_dim: int)
 
 
 @pytest.mark.parametrize(
-    'seq_length,embed_dim',
+    'seq_length, embed_dim',
     (
         (74, 512),
         (128, 1024),
@@ -44,19 +48,23 @@ def test_cross_attention(query_length: int, context_length: int, embed_dim: int)
 )
 def test_global_attention(seq_length: int, embed_dim: int) -> None:
     """Check the shape of the self attention layer's output."""
-    batch_size = 32
     # Create dummy embedding.
-    embedding = tf.random.normal((batch_size, seq_length, embed_dim))
+    embedding = tf.keras.Input(
+        shape=(seq_length, embed_dim),
+        dtype=tf.float32,
+    )
 
     # Create self attention layer with 2 attention heads.
     gsa = GlobalSelfAttention(num_heads=2, key_dim=embed_dim)
     output = gsa(query=embedding)
 
-    assert output.shape == (batch_size, seq_length, embed_dim)
+    # Expected output shape.
+    expected_shape = tf.TensorShape((None, seq_length, embed_dim))
+    assert output.shape == expected_shape
 
 
 @pytest.mark.parametrize(
-    'seq_length,embed_dim',
+    'seq_length, embed_dim',
     (
         (64, 256),
         (512, 128),
@@ -64,14 +72,16 @@ def test_global_attention(seq_length: int, embed_dim: int) -> None:
 )
 def test_casual_attention(seq_length: int, embed_dim: int) -> None:
     """Test casual self-attention layer's output shape."""
-    batch_size = 32
     # Create a dummy embedding.
-    embedding = tf.random.normal(
-        shape=(batch_size, seq_length, embed_dim),
+    embedding = tf.keras.Input(
+        shape=(seq_length, embed_dim),
+        dtype=tf.float32,
     )
 
     # Create casual self attention layer with 2 attention heads.
     csa = CausalSelfAttention(num_heads=2, key_dim=embed_dim)
     output = csa(embedding)
 
-    assert output.shape == (batch_size, seq_length, embed_dim)
+    # Expected output shape.
+    expected_shape = tf.TensorShape((None, seq_length, embed_dim))
+    assert output.shape == expected_shape
